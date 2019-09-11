f1861413652aa2b02f6f15c86d35231ad8d922ac
John Kim <13jkim@gmail.com>
2019-09-10 22:00:02 -0400

    header and random

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,267 +10,267 @@ import Sidebar from '../components/Sidebar';
 import Scroll from '../components/Scroll';
 
 const IndexPage = () => (
-  <Layout>
-    <Sidebar />
+	<Layout>
+		<Sidebar />
 
-    <div id="wrapper">
-      <section id="intro" className="wrapper style1 fullscreen fade-up">
-        <div className="inner">
-          <h1>Metro Church</h1>
-          <p>
-            Renewing. 
-            <br />
-            Rebuilding. 
-            <br /> 
-            Retraining.
-            <br />
-            Open-Source starter coded in Gatsby on top of Html5Up Hyperspace design 
-            <a href="https://github.com/app-generator/gatsby-html5up-hyperspace">Get Sources</a> 
-          </p>
-          <ul className="actions">
-            <li>
-              <Scroll type="id" element="one">
-                <a href="#one" className="button">
-                  Learn more
-                </a>
-              </Scroll>
-            </li>
-          </ul>
-        </div>
-      </section>
+		<div id="wrapper">
+			<section id="intro" className="wrapper style1 fullscreen fade-up">
+				<div className="inner">
+					<h1>Metro Church</h1>
+					<p>
+						Renewing. 
+						<br />
+						Rebuilding. 
+						<br /> 
+						Retraining.
+						<br />
+						Open-Source starter coded in Gatsby on top of Html5Up Hyperspace design 
+						<a href="https://github.com/app-generator/gatsby-html5up-hyperspace">Get Sources</a> 
+					</p>
+					<ul className="actions">
+						<li>
+							<Scroll type="id" element="one">
+								<a href="#one" className="button">
+									Learn more
+								</a>
+							</Scroll>
+						</li>
+					</ul>
+				</div>
+			</section>
 
-      <section id="one" className="wrapper style2 spotlights">
-        <section>
-          <a href="/#" className="image" alt="image">
-            <img src={pic1} alt="" data-position="center center" />
-          </a>
-          <div className="content">
-            <div className="inner">
-              <h2>Sed ipsum dolor</h2>
-              <p>
-                Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-                turpis mauris, eu ultricies erat malesuada quis. Aliquam
-                dapibus.
-              </p>
-              <ul className="actions">
-                <li>
-                  <Link className="button" to="/generic">
-                    Learn more
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
-        <section>
-          <a href="/#" className="image">
-            <img src={pic2} alt="" data-position="top center" />
-          </a>
-          <div className="content">
-            <div className="inner">
-              <h2>Feugiat consequat</h2>
-              <p>
-                Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-                turpis mauris, eu ultricies erat malesuada quis. Aliquam
-                dapibus.
-              </p>
-              <ul className="actions">
-                <li>
-                  <Link className="button" to="/generic">
-                    Learn more
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
-        <section>
-          <a href="/#" className="image">
-            <img src={pic3} alt="" data-position="25% 25%" />
-          </a>
-          <div className="content">
-            <div className="inner">
-              <h2>Ultricies aliquam</h2>
-              <p>
-                Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-                turpis mauris, eu ultricies erat malesuada quis. Aliquam
-                dapibus.
-              </p>
-              <ul className="actions">
-                <li>
-                  <Link className="button" to="/generic">
-                    Learn more
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </section>
-      </section>
+			<section id="one" className="wrapper style2 spotlights">
+				<section>
+					<a href="/#" className="image" alt="image">
+						<img src={pic1} alt="" data-position="center center" />
+					</a>
+					<div className="content">
+						<div className="inner">
+							<h2>Sed ipsum dolor</h2>
+							<p>
+								Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+								turpis mauris, eu ultricies erat malesuada quis. Aliquam
+								dapibus.
+							</p>
+							<ul className="actions">
+								<li>
+									<Link className="button" to="/generic">
+										Learn more
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</section>
+				<section>
+					<a href="/#" className="image">
+						<img src={pic2} alt="" data-position="top center" />
+					</a>
+					<div className="content">
+						<div className="inner">
+							<h2>Feugiat consequat</h2>
+							<p>
+								Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+								turpis mauris, eu ultricies erat malesuada quis. Aliquam
+								dapibus.
+							</p>
+							<ul className="actions">
+								<li>
+									<Link className="button" to="/generic">
+										Learn more
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</section>
+				<section>
+					<a href="/#" className="image">
+						<img src={pic3} alt="" data-position="25% 25%" />
+					</a>
+					<div className="content">
+						<div className="inner">
+							<h2>Ultricies aliquam</h2>
+							<p>
+								Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+								turpis mauris, eu ultricies erat malesuada quis. Aliquam
+								dapibus.
+							</p>
+							<ul className="actions">
+								<li>
+									<Link className="button" to="/generic">
+										Learn more
+									</Link>
+								</li>
+							</ul>
+						</div>
+					</div>
+				</section>
+			</section>
 
-      <section id="two" className="wrapper style3 fade-up">
-        <div className="inner">
-          <h2>What we do</h2>
-          <p>
-            Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-            turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus,
-            lacus eget hendrerit bibendum, urna est aliquam sem, sit amet
-            imperdiet est velit quis lorem.
-          </p>
-          <div className="features">
-            <section>
-              <span className="icon major fa-code" />
-              <h3>Lorem ipsum amet</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-lock" />
-              <h3>Aliquam sed nullam</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-cog" />
-              <h3>Sed erat ullam corper</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-desktop" />
-              <h3>Veroeros quis lorem</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-chain" />
-              <h3>Urna quis bibendum</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-            <section>
-              <span className="icon major fa-diamond" />
-              <h3>Aliquam urna dapibus</h3>
-              <p>
-                Phasellus convallis elit id ullam corper amet et pulvinar. Duis
-                aliquam turpis mauris, sed ultricies erat dapibus.
-              </p>
-            </section>
-          </div>
-          <ul className="actions">
-            <li>
-              <Link className="button" to="/generic">
-                Learn more
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </section>
+			<section id="two" className="wrapper style3 fade-up">
+				<div className="inner">
+					<h2>What we do</h2>
+					<p>
+						Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+						turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus,
+						lacus eget hendrerit bibendum, urna est aliquam sem, sit amet
+						imperdiet est velit quis lorem.
+					</p>
+					<div className="features">
+						<section>
+							<span className="icon major fa-code" />
+							<h3>Lorem ipsum amet</h3>
+							<p>
+								Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+								aliquam turpis mauris, sed ultricies erat dapibus.
+							</p>
+						</section>
+						<section>
+							<span className="icon major fa-lock" />
+							<h3>Aliquam sed nullam</h3>
+							<p>
+								Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+								aliquam turpis mauris, sed ultricies erat dapibus.
+							</p>
+						</section>
+						<section>
+							<span className="icon major fa-cog" />
+							<h3>Sed erat ullam corper</h3>
+							<p>
+								Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+								aliquam turpis mauris, sed ultricies erat dapibus.
+							</p>
+						</section>
+						<section>
+							<span className="icon major fa-desktop" />
+							<h3>Veroeros quis lorem</h3>
+							<p>
+								Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+								aliquam turpis mauris, sed ultricies erat dapibus.
+							</p>
+						</section>
+						<section>
+							<span className="icon major fa-chain" />
+							<h3>Urna quis bibendum</h3>
+							<p>
+								Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+								aliquam turpis mauris, sed ultricies erat dapibus.
+							</p>
+						</section>
+						<section>
+							<span className="icon major fa-diamond" />
+							<h3>Aliquam urna dapibus</h3>
+							<p>
+								Phasellus convallis elit id ullam corper amet et pulvinar. Duis
+								aliquam turpis mauris, sed ultricies erat dapibus.
+							</p>
+						</section>
+					</div>
+					<ul className="actions">
+						<li>
+							<Link className="button" to="/generic">
+								Learn more
+							</Link>
+						</li>
+					</ul>
+				</div>
+			</section>
 
-      <section id="three" className="wrapper style1 fade-up">
-        <div className="inner">
-          <h2>Get in touch</h2>
-          <p>
-            Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
-            turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus,
-            lacus eget hendrerit bibendum, urna est aliquam sem, sit amet
-            imperdiet est velit quis lorem.
-          </p>
-          <div className="split style1">
-            <section>
-              <form method="post" action="#">
-                <div className="fields">
-                  <div className="field half">
-                    <label for="name">Name</label>
-                    <input type="text" name="name" id="name" />
-                  </div>
-                  <div className="field half">
-                    <label for="email">Email</label>
-                    <input type="text" name="email" id="email" />
-                  </div>
-                  <div className="field">
-                    <label for="message">Message</label>
-                    <textarea name="message" id="message" rows="5" />
-                  </div>
-                </div>
-                <ul className="actions">
-                  <li>
-                    <a href="/#" className="button submit">
-                      Send Message
-                    </a>
-                  </li>
-                </ul>
-              </form>
-            </section>
-            <section>
-              <ul className="contact">
-                <li>
-                  <h3>Address</h3>
-                  <span>
-                    12345 Somewhere Road #654
-                    <br />
-                    Nashville, TN 00000-0000
-                    <br />
-                    USA
-                  </span>
-                </li>
-                <li>
-                  <h3>Email</h3>
-                  <a href="/#">user@untitled.tld</a>
-                </li>
-                <li>
-                  <h3>Phone</h3>
-                  <span>(000) 000-0000</span>
-                </li>
-                <li>
-                  <h3>Social</h3>
-                  <ul className="icons">
-                    <li>
-                      <a href="/#" className="fa-twitter">
-                        <span className="label">Twitter</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="/#" className="fa-facebook">
-                        <span className="label">Facebook</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="/#" className="fa-github">
-                        <span className="label">GitHub</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="/#" className="fa-instagram">
-                        <span className="label">Instagram</span>
-                      </a>
-                    </li>
-                    <li>
-                      <a href="/#" className="fa-linkedin">
-                        <span className="label">LinkedIn</span>
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </section>
-          </div>
-        </div>
-      </section>
-    </div>
+			<section id="three" className="wrapper style1 fade-up">
+				<div className="inner">
+					<h2>Get in touch</h2>
+					<p>
+						Phasellus convallis elit id ullamcorper pulvinar. Duis aliquam
+						turpis mauris, eu ultricies erat malesuada quis. Aliquam dapibus,
+						lacus eget hendrerit bibendum, urna est aliquam sem, sit amet
+						imperdiet est velit quis lorem.
+					</p>
+					<div className="split style1">
+						<section>
+							<form method="post" action="#">
+								<div className="fields">
+									<div className="field half">
+										<label for="name">Name</label>
+										<input type="text" name="name" id="name" />
+									</div>
+									<div className="field half">
+										<label for="email">Email</label>
+										<input type="text" name="email" id="email" />
+									</div>
+									<div className="field">
+										<label for="message">Message</label>
+										<textarea name="message" id="message" rows="5" />
+									</div>
+								</div>
+								<ul className="actions">
+									<li>
+										<a href="/#" className="button submit">
+											Send Message
+										</a>
+									</li>
+								</ul>
+							</form>
+						</section>
+						<section>
+							<ul className="contact">
+								<li>
+									<h3>Address</h3>
+									<span>
+										12345 Somewhere Road #654
+										<br />
+										Nashville, TN 00000-0000
+										<br />
+										USA
+									</span>
+								</li>
+								<li>
+									<h3>Email</h3>
+									<a href="/#">user@untitled.tld</a>
+								</li>
+								<li>
+									<h3>Phone</h3>
+									<span>(000) 000-0000</span>
+								</li>
+								<li>
+									<h3>Social</h3>
+									<ul className="icons">
+										<li>
+											<a href="/#" className="fa-twitter">
+												<span className="label">Twitter</span>
+											</a>
+										</li>
+										<li>
+											<a href="/#" className="fa-facebook">
+												<span className="label">Facebook</span>
+											</a>
+										</li>
+										<li>
+											<a href="/#" className="fa-github">
+												<span className="label">GitHub</span>
+											</a>
+										</li>
+										<li>
+											<a href="/#" className="fa-instagram">
+												<span className="label">Instagram</span>
+											</a>
+										</li>
+										<li>
+											<a href="/#" className="fa-linkedin">
+												<span className="label">LinkedIn</span>
+											</a>
+										</li>
+									</ul>
+								</li>
+							</ul>
+						</section>
+					</div>
+				</div>
+			</section>
+		</div>
 
-    <Footer />
-  </Layout>
+		<Footer />
+	</Layout>
 );
 
 export default IndexPage;
---
+++ src/pages/sermons.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Header from '../components/Header'
+
+const Sermons = () => (
+	<Header />
+)
+
+export default Sermons;
